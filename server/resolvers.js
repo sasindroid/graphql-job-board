d85fb72c758e53db6b9b781db5c71cade278a5f4
Sasikumar Lakshmanan <sasikumar@giffgaff.co.uk>
2021-09-04 21:54:06 +0530

    added nested company type to resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,13 @@
+const db = require('./db');
+
 const Query = {
-  greeting: () => 'Hello Sasi'
+  jobs: () => db.jobs.list(),
 };
 
-module.exports = { Query };
+const Job = {
+  // each resolver receives an argument, in this case
+  // 1st arg: parent object
+  company: (job) => db.companies.get(job.companyId),
+};
+
+module.exports = { Query, Job };
